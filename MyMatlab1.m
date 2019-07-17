15571c768658d45441854836f061e7f780bec565
gersusa <43012424+gersusa@users.noreply.github.com>
2019-07-17 10:18:08 -0500

    Add files via upload

--- MyMatlab1.m
+++ MyMatlab1.m
@@ -12,7 +12,7 @@ function MyMatlab1(InFile1, InFile2, InFile3, InFile4, ...
 
 
 fprintf('Started\n')
-parpool('local',4);
+parpool('local',16);
 %parpool('GOC_SlurmProfile1',16);
 [mpc,contingencies] = convert2mpc(InFile3,InFile4,...
                                                    InFile2,InFile1);
